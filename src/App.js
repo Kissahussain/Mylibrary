7ae57fd09ba3f2a4f650b1a8d569883f67f3def3
kissa hussain <kissahussain@gmail.com>
2024-12-07 17:25:02 -0600

    edit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>My Library</code> and save to reload.
+          Edit <code>MyLibrary</code> and save to reload.
         </p>
         <a
           className="App-link"
